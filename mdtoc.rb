0d893594e1d56c8a2336372265c5bd045a439d44
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-28 18:15:13 +0900

    Brew formula update for mdtoc version v0.0.5

--- mdtoc.rb
+++ mdtoc.rb
@@ -1,9 +1,9 @@
 class Mdtoc < Formula
   desc "CLI tool to Generate and insert markdown's table of contents"
   homepage "https://github.com/takaishi/mdtoc"
-  url "https://github.com/takaishi/mdtoc/releases/download/v0.0.4/mdtoc_0.0.4_Darwin_x86_64.tar.gz"
-  version "0.0.4"
-  sha256 "032dc92f71459c8a80907a908b7cfcbf2ee202d7afc14fbf84742efda8aac673"
+  url "https://github.com/takaishi/mdtoc/releases/download/v0.0.5/mdtoc_0.0.5_Darwin_x86_64.tar.gz"
+  version "0.0.5"
+  sha256 "e3a79082d2d68b7f1716444fceaa0a4415a5aa5952f14dc97786b854950c38f0"
 
   def install
     bin.install Dir['mdtoc']
