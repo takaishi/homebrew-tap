e5e9ed0b70e0734687abd264a4033d0eb820cc54
r_takaishi <ryo.takaishi.0@gmail.com>
2019-01-28 16:53:43 +0900

    space!!!!!!!

--- mdtoc.rb
+++ mdtoc.rb
@@ -5,7 +5,7 @@ class Mdtoc < Formula
   desc "CLI tool to create text file from template and manage templates."
   homepage ""
   url "https://github.com/takaishi/mdtoc/releases/download/v0.0.1/mdtoc_0.0.1_darwin_amd64.zip"
-  sha256 "0f13dcf7d6ad7d4c881780c672d1c7b5948ade4c65947468998b589e608c0b6b "
+  sha256 "0f13dcf7d6ad7d4c881780c672d1c7b5948ade4c65947468998b589e608c0b6b"
 
   def install
     bin.install 'mdtoc'
