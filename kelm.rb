f8e9ca26e617645d51d173a6399a7c1a1dbb1283
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-08 14:44:54 +0900

    Brew formula update for kelm version v0.0.4

--- kelm.rb
+++ kelm.rb
@@ -2,16 +2,16 @@
 class Kelm < Formula
   desc "CLI tool to Generate and insert markdown's table of contents"
   homepage "https://github.com/takaishi/kelm"
-  version "0.0.3"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/takaishi/kelm/releases/download/v0.0.3/kelm_0.0.3_Darwin_x86_64.tar.gz"
-    sha256 "680c83b1b424bdd23e2f2004f001b7680d4b6b0df4d555b1e24c8b033422f2f9"
+    url "https://github.com/takaishi/kelm/releases/download/v0.0.4/kelm_0.0.4_Darwin_x86_64.tar.gz"
+    sha256 "8a224703d225ffb8c0570dc25240041421d6f483d0075ab8d2cf04d19152986b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/takaishi/kelm/releases/download/v0.0.3/kelm_0.0.3_Linux_x86_64.tar.gz"
-      sha256 "975b752d2ca951c30897f70a89ad2959a09a3efd1410cc712ef19ec2f6310214"
+      url "https://github.com/takaishi/kelm/releases/download/v0.0.4/kelm_0.0.4_Linux_x86_64.tar.gz"
+      sha256 "176591b972155fec7e91c56d9cdf7d8c1bc1805b9928a8cfda350a999156e87b"
     end
   end
 
