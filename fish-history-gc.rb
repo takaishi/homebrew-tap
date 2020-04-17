2adab63ad9a8e3b345a5be5c3b92cd808ef0cfa3
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-17 18:43:30 +0900

    Brew formula update for fish-history-gc version v0.0.9

--- fish-history-gc.rb
+++ fish-history-gc.rb
@@ -2,16 +2,16 @@
 class FishHistoryGc < Formula
   desc "CLI tool to Generate and insert markdown's table of contents"
   homepage "https://github.com/takaishi/fish-history-gc"
-  version "0.0.8"
+  version "0.0.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/takaishi/fish-history-gc/releases/download/v0.0.8/fish-history-gc_0.0.8_Darwin_x86_64.tar.gz"
-    sha256 "3cf4e953819fbb3c8db22d6dab89d29a73cfc1d57e37500de606a5e7e4d48d26"
+    url "https://github.com/takaishi/fish-history-gc/releases/download/v0.0.9/fish-history-gc_0.0.9_Darwin_x86_64.tar.gz"
+    sha256 "c15909fe418a3f701f36f5cc3c91e80c6f18bc11bcf7e0498643a9a336a162a9"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/takaishi/fish-history-gc/releases/download/v0.0.8/fish-history-gc_0.0.8_Linux_x86_64.tar.gz"
-      sha256 "011b2f4715fe70ffe47c4dcc706d8924dcd857c64d3e668103590f6b1c5064ec"
+      url "https://github.com/takaishi/fish-history-gc/releases/download/v0.0.9/fish-history-gc_0.0.9_Linux_x86_64.tar.gz"
+      sha256 "226e83d05bad44bf6a32fd3f3604e99781354b084586b682960d9a07b8f35076"
     end
   end
 
